35181144476d607e24bec58e6e400ccc5501d7d7
Dongchen Cui <1663523296@qq.com>
2025-07-15 15:16:31 +0800

    01 try build kernel

--- Labfiles/01-build-your-kernel/Python/kernel.py
+++ Labfiles/01-build-your-kernel/Python/kernel.py
@@ -3,7 +3,9 @@ import asyncio
 from dotenv import load_dotenv
 
 # Import namespaces
-
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.functions.kernel_arguments import KernelArguments
 
 
 async def main():
@@ -16,9 +18,20 @@ async def main():
 
 
     # Create a kernel with Azure OpenAI chat completion
-    
+    kernel = Kernel()
+    chat_completion = AzureChatCompletion(
+        api_key=api_key,
+        endpoint=endpoint,
+        deployment_name=deployment_name
+    )
+    kernel.add_service(chat_completion)
 
     # Test the chat completion service
+    response = await kernel.invoke_prompt(
+        "Give me a list of 10 breakfast foods with eggs and cheese", 
+        KernelArguments())
+    print("Assistant > " + str(response))
+
 
 if __name__ == "__main__":
         asyncio.run(main())
